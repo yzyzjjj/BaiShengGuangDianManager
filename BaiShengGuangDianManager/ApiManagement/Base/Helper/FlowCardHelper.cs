712fa758b2a606800d3dd480f0142362a09c21c2
wanghui <you@example.com>
2019-06-13 12:29:32 +0800

    fc

--- BaiShengGuangDianManager/ApiManagement/Base/Helper/FlowCardHelper.cs
+++ BaiShengGuangDianManager/ApiManagement/Base/Helper/FlowCardHelper.cs
@@ -20,10 +20,19 @@ namespace ApiManagement.Base.Helper
         private static DateTime _starTime = DateTime.Now.DayBeginTime();
         private static string _createUserId = "ErpSystem";
         private static string _url = "";
+        private static Dictionary<string, string> _processStepName = new Dictionary<string, string>
+        {
+            {"线切割", "线切割"},
+            {"精磨", "精磨"},
+            {"Polish", "粗抛"},
+            {"抛光", "粗抛"},
+            {"粗抛", "粗抛"},
+            {"精抛", "精抛"},
+        };
         public static void Init(IConfiguration configuration)
         {
             _url = configuration.GetAppSettings<string>("ErpUrl");
-            _time = new Timer(Call, null, 10000, 1000 * 60 * 1);
+            _time = new Timer(Call, null, 5000, 1000 * 60999 * 1);
         }
         private static void Call(object state)
         {
@@ -35,7 +44,7 @@ namespace ApiManagement.Base.Helper
             }
             var queryTime1 = _starTime;
             var queryTime2 = DateTime.Now;
-            var r = GetData(queryTime1.AddMinutes(-10), queryTime2);
+            var r = GetData(queryTime1, queryTime2);
             _starTime = !r ? queryTime1 : queryTime2;
         }
 
@@ -62,12 +71,28 @@ namespace ApiManagement.Base.Helper
             public List<ErpFlowCard> data;
             public ErpRelation[] relation;
         }
+
+
+        /// <summary>
+        /// 计划号工序
+        /// </summary>
+        public class ErpGx
+        {
+            public string n;
+            public string v;
+        }
+        public class ErpJhhGx
+        {
+            public string jhh;
+            public ErpGx[] gx;
+        }
         private static bool GetData(DateTime starTime, DateTime endTime)
         {
             var f = HttpServer.Get(_url, new Dictionary<string, string>
             {
-                {"t1", starTime.ToStr()},
-                {"t2", endTime.ToStr()},
+                { "type", "getHairpin" },
+                { "t1", starTime.ToStr()},
+                { "t2", endTime.ToStr()},
             });
             if (f == "fail")
             {
@@ -82,7 +107,7 @@ namespace ApiManagement.Base.Helper
                 var res = JsonConvert.DeserializeObject<ErpRes>(rr);
                 if (res.result != "ok")
                 {
-                    Log.ErrorFormat("请求erp返回失败,原因:{0}", res.result);
+                    Log.ErrorFormat("请求erp获取流程卡数据返回错误,原因:{0}", res.result);
                     return false;
                 }
 
@@ -120,7 +145,7 @@ namespace ApiManagement.Base.Helper
 
                 //原料批号
                 var rawMaterias = ServerConfig.ApiDb.Query<RawMateria>("SELECT * FROM `raw_materia`;").ToDictionary(x => x.RawMateriaName);
-                var erpRawMaterias = r.GroupBy(x => x.f_mate);
+                var erpRawMaterias = r.GroupBy(x => x.f_mate).ToDictionary(x => x.Key);
                 var newRawMaterias = erpRawMaterias.Where(x => !rawMaterias.ContainsKey(x.Key));
                 var newRm = newRawMaterias.Select(x => new RawMateria
                 {
@@ -134,7 +159,7 @@ namespace ApiManagement.Base.Helper
                     newRm);
                 rawMaterias = ServerConfig.ApiDb.Query<RawMateria>("SELECT * FROM `raw_materia`;").ToDictionary(x => x.RawMateriaName);
 
-                //原料批号
+                //计划号
                 var productionLibraries = ServerConfig.ApiDb.Query<ProductionLibrary>("SELECT * FROM `production_library`;").ToDictionary(x => x.ProductionProcessName);
                 var erpProductionLibraries = r.GroupBy(x => x.f_jhh);
                 var newProductionLibraries = erpProductionLibraries.Where(x => !productionLibraries.ContainsKey(x.Key));
@@ -144,11 +169,77 @@ namespace ApiManagement.Base.Helper
                     MarkedDateTime = now,
                     ProductionProcessName = x.Key
                 });
+                var newPlTmp = new List<ProductionLibrary>();
+                newPlTmp.AddRange(newPl);
                 ServerConfig.ApiDb.Execute(
                     "INSERT INTO production_library (`CreateUserId`, `MarkedDateTime`, `MarkedDelete`, `ModifyId`, `ProductionProcessName`) " +
                     "VALUES (@CreateUserId, @MarkedDateTime, @MarkedDelete, @ModifyId, @ProductionProcessName);",
                     newPl);
                 productionLibraries = ServerConfig.ApiDb.Query<ProductionLibrary>("SELECT * FROM `production_library`;").ToDictionary(x => x.ProductionProcessName);
+
+                //数据库计划号工序
+                var productionProcessStep = ServerConfig.ApiDb.Query<ProductionProcessStepDetail>("SELECT a.*, b.ProductionProcessName FROM `production_process_step` a JOIN `production_library` b ON a.ProductionProcessId = b.Id; ")
+                    .GroupBy(x => x.ProductionProcessId).ToDictionary(x => x.Key);
+                //获取的流程卡用到的计划号
+                if (erpProductionLibraries.Any())
+                {
+                    var ff = HttpServer.Get(_url, new Dictionary<string, string>
+                    {
+                        { "type", "getProcessParameters" },
+                        { "jhh", erpProductionLibraries.Select(x=>x.Key).Join(",")},
+                    });
+                    if (ff == "fail")
+                    {
+                        Log.ErrorFormat("请求erp获取计划号工序数据失败,url:{0}", _url);
+                        return false;
+                    }
+                    var rrr = HttpUtility.UrlDecode(ff);
+                    var erpJhhGxes = JsonConvert.DeserializeObject<ErpJhhGx[]>(rrr);
+                    if (erpJhhGxes.Any())
+                    {
+                        var deviceProcessSteps = ServerConfig.ApiDb.Query<DeviceProcessStep>("SELECT Id, StepName FROM `device_process_step` WHERE MarkedDelete = 0;");
+
+                        //erp计划号工序
+                        var erpProductionProcessStep = erpJhhGxes.ToDictionary(x => x.jhh);
+                        //var newProductionProcessStep = erpProductionProcessStep.Where(x => !productionProcessStep.ContainsKey(x.Key));
+
+                        //计划号新工序
+                        var newPps = new List<ProductionProcessStep>();
+                        foreach (var pl in newPlTmp)
+                        {
+                            if (erpProductionProcessStep.ContainsKey(pl.ProductionProcessName))
+                            {
+                                var newProductionProcessStep = erpProductionProcessStep[pl.ProductionProcessName].gx;
+                                var i = 1;
+                                foreach (var processStep in newProductionProcessStep)
+                                {
+                                    var n = _processStepName.ContainsKey(processStep.n)
+                                        ? _processStepName[processStep.n]
+                                        : processStep.n;
+
+                                    var dps = deviceProcessSteps.FirstOrDefault(x => x.StepName == n);
+                                    newPps.Add(new ProductionProcessStep
+                                    {
+                                        CreateUserId = _createUserId,
+                                        MarkedDateTime = now,
+                                        ProductionProcessId = productionLibraries[pl.ProductionProcessName].Id,
+                                        ProcessStepOrder = i++,
+                                        ProcessStepId = dps?.Id ?? 0,
+                                        ProcessStepRequirements = processStep.v,
+                                        ProcessStepRequirementMid = GetMid(processStep.v)
+                                    });
+                                }
+                            }
+                        }
+                        ServerConfig.ApiDb.Execute(
+                            "INSERT INTO production_process_step (`CreateUserId`, `MarkedDateTime`, `MarkedDelete`, `ModifyId`, `ProductionProcessId`, `ProcessStepOrder`, `ProcessStepId`, `ProcessStepRequirements`, `ProcessStepRequirementMid`) " +
+                            "VALUES (@CreateUserId, @MarkedDateTime, @MarkedDelete, @ModifyId, @ProductionProcessId, @ProcessStepOrder, @ProcessStepId, @ProcessStepRequirements, @ProcessStepRequirementMid);",
+                            newPps);
+
+                        productionProcessStep = ServerConfig.ApiDb.Query<ProductionProcessStepDetail>("SELECT a.*, b.ProductionProcessName FROM `production_process_step` a JOIN `production_library` b ON a.ProductionProcessId = b.Id; ")
+                          .GroupBy(x => x.ProductionProcessId).ToDictionary(x => x.Key);
+                    }
+                }
 
                 //流程卡
                 var flowCardLibraries = ServerConfig.ApiDb.Query<FlowCardLibrary>("SELECT * FROM `flowcard_library`;").ToDictionary(x => x.FlowCardName);
@@ -164,10 +255,48 @@ namespace ApiManagement.Base.Helper
                     CreateTime = x.Value.f_inserttime,
                     WorkshopId = x.Value.f_bz
                 });
+                var newFcTmp = new List<FlowCardLibrary>();
+                newFcTmp.AddRange(newFc);
                 ServerConfig.ApiDb.Execute(
                     "INSERT INTO flowcard_library (`CreateUserId`, `MarkedDateTime`, `MarkedDelete`, `ModifyId`, `FlowCardName`, `ProductionProcessId`, `RawMateriaId`, `RawMaterialQuantity`, `Sender`, `InboundNum`, `Remarks`, `Priority`, `CreateTime`, `WorkshopId`) " +
                     "VALUES (@CreateUserId, @MarkedDateTime, @MarkedDelete, @ModifyId, @FlowCardName, @ProductionProcessId, @RawMateriaId, @RawMaterialQuantity, @Sender, @InboundNum, @Remarks, @Priority, @CreateTime, @WorkshopId);",
                     newFc);
+
+                flowCardLibraries = ServerConfig.ApiDb.Query<FlowCardLibrary>("SELECT * FROM `flowcard_library`;").ToDictionary(x => x.FlowCardName);
+                newFc = flowCardLibraries.Values.Where(x => newFcTmp.Any(y => y.FlowCardName == x.FlowCardName));
+                if (productionProcessStep.Any())
+                {
+                    //流程卡新工序
+                    var newFps = new List<FlowCardProcessStep>();
+                    foreach (var fc in newFc)
+                    {
+                        if (!productionProcessStep.ContainsKey(fc.ProductionProcessId))
+                        {
+                            continue;
+                        }
+
+                        var i = 1;
+                        var newFlowCardProcessStep = productionProcessStep[fc.ProductionProcessId];
+                        foreach (var processStep in newFlowCardProcessStep)
+                        {
+                            newFps.Add(new FlowCardProcessStep
+                            {
+                                CreateUserId = _createUserId,
+                                MarkedDateTime = now,
+                                FlowCardId = fc.Id,
+                                ProcessStepOrder = processStep.ProcessStepOrder,
+                                ProcessStepId = processStep.ProcessStepId,
+                                ProcessStepRequirements = processStep.ProcessStepRequirements,
+                                ProcessStepRequirementMid = processStep.ProcessStepRequirementMid,
+                            });
+                        }
+                    }
+
+                    ServerConfig.ApiDb.Execute(
+                        "INSERT INTO flowcard_process_step (`CreateUserId`, `MarkedDateTime`, `MarkedDelete`, `ModifyId`, `FlowCardId`, `ProcessStepOrder`, `ProcessStepId`, `ProcessStepRequirements`, `ProcessStepRequirementMid`) " +
+                        "VALUES (@CreateUserId, @MarkedDateTime, @MarkedDelete, @ModifyId, @FlowCardId, @ProcessStepOrder, @ProcessStepId, @ProcessStepRequirements, @ProcessStepRequirementMid);",
+                        newFps.OrderBy(x => x.FlowCardId).ThenBy(x => x.ProcessStepOrder));
+                }
             }
             catch (Exception e)
             {
@@ -176,6 +305,101 @@ namespace ApiManagement.Base.Helper
             }
             return true;
 
+        }
+
+        public static decimal GetMid(string value)
+        {
+            if (value == "")
+            {
+                return 0;
+            }
+
+            value += ";";
+            var t = 0;
+            if (value.Contains("±"))
+            {
+                //0.255±0.005mm
+                t = 1;
+            }
+            else if (value.Contains("～"))
+            {
+                //0.202～0.216mm
+                t = 2;
+            }
+            else if (value.Contains("+") && value.Contains("-"))
+            {
+                //0.21+0.013/-0.027mm
+                t = 3;
+            }
+
+            var num = new List<decimal>();
+            var s = value.Replace(" ", "").Select(x => x.ToString()).ToArray();
+            var n = "";
+            foreach (var sf in s)
+            {
+                if (int.TryParse(sf, out _) || sf == ".")
+                {
+                    n += sf;
+                }
+                else
+                {
+                    if (decimal.TryParse(n, out var k))
+                    {
+                        num.Add(k);
+                    }
+
+                    n = "";
+                }
+            }
+
+            decimal p = 0;
+            switch (t)
+            {
+                case 1:
+                    if (num.Count >= 1)
+                    {
+                        p = num[0];
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                case 2:
+                    if (num.Count == 1)
+                    {
+                        p = num[0]; break;
+                    }
+                    else if (num.Count >= 2)
+                    {
+                        p = (num[0] + num[1]) / 2; break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                case 3:
+                    if (num.Count == 1)
+                    {
+                        p = num[0]; break;
+                    }
+                    else if (num.Count >= 3)
+                    {
+                        p = num[0] + (num[1] + num[2]) / 2; break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                default:
+                    if (num.Count >= 1)
+                    {
+                        p = num[0];
+                    }
+                    break;
+            }
+
+            return p.ToRound(4);
         }
     }
 }
