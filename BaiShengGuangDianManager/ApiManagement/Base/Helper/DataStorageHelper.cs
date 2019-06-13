d9d8f44366f243ec7a491456416082b6465fcd14
wanghui <you@example.com>
2019-06-13 12:03:30 +0800

    fc

---
+++ BaiShengGuangDianManager/ApiManagement/Base/Helper/DataStorageHelper.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ApiManagement.Base.Helper
+{
+    public class DataStorageHelper
+    {
+        private static Thread _analysis;
+
+
+        public static void Init()
+        {
+
+
+
+            _analysis = new Thread(Analysis);
+        }
+
+        private static void Analysis()
+        {
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
